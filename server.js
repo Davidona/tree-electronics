c74245d21c024663960489bdfb3ee9f6c09050d0
Bradley-Feitsvaig <Bradley.Feitsvaig@gmail.com>
2022-05-14 16:04:38 +0300

    organized project

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+var path = require('path');
+
+app.use(express.static(__dirname));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/html/dashboard.html'));
+})
+
+app.get('/sign-in', function (req, res) {
+    res.sendFile(path.join(__dirname + '/html/sign-in.html'));
+})
+
+app.get('/sign-up', function (req, res) {
+    res.sendFile(path.join(__dirname + '/html/sign-up.html'));
+})
+
+app.get('/contact-us', function (req, res) {
+    res.sendFile(path.join(__dirname + '/html/contact-us-page.html'));
+})
+
+app.get('/reset-password', function (req, res) {
+    res.sendFile(path.join(__dirname + '/html/reset-password.html'));
+})
+
+app.get('/update-password', function (req, res) {
+    res.sendFile(path.join(__dirname + '/html/reset-password.html'));
+})
+
+app.listen(port);
+console.log('Server started! At http://localhost:' + port);
