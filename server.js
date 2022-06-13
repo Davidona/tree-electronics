dd2043dc8c17b2b6996aeed74e8d63718009430c
Bradley-Feitsvaig <Bradley.Feitsvaig@gmail.com>
2022-06-13 11:11:26 +0300

    change local host port to 3000

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 4000;
+var port = process.env.PORT || 3000;
 var path = require('path');
 const db = require('./queries')
 
