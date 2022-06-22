72a67ffe753b898f1f9f4db75ea134fd34e00f65
Bradley-Feitsvaig <Bradley.Feitsvaig@gmail.com>
2022-06-22 20:10:47 +0300

    make it better

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const {
   pool
 } = require("./dbConfig");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const passport = require("passport");
 const flash = require("express-flash");
 const session = require("express-session");
