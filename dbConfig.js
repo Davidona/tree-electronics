b9177e7617b926a29fcd1892cc2cf83390d81d8d
Davidona <81252480+Davidona@users.noreply.github.com>
2022-06-22 21:38:18 +0300

    Update dbConfig.js

--- dbConfig.js
+++ dbConfig.js
@@ -1,12 +1,12 @@
-require("dotenv").config();
+require("dotenv").config();https://github.com/Davidona/tree-electronics/blob/master/dbConfig.js
 
 const { Pool } = require("pg");
 
 const pool = new Pool({
-    user: 'dfhhzifanwocgt',
-    host: 'ec2-54-170-90-26.eu-west-1.compute.amazonaws.com',
-    database: 'dfd28959ibbmsv',
-    password: '7289851ff7f8ec85fabd4e539f334873ac3dff791f6336e219c75753050cb8e8',
+    user: '',
+    host: '',
+    database: '',
+    password: '',
     port: 5432,
     ssl: {
         rejectUnauthorized: false
